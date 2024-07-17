3a3e2d17175170593a5fce382df7f4a92aa49fa3
oscardguzman92 <38897817+oscardguzman92@users.noreply.github.com>
2024-07-16 21:35:59 -0500

    delete comments

--- SOAPPokemonService/src/main/java/com/PokemonApp/SOAPPokemonService/application/service/PokemonService.java
+++ SOAPPokemonService/src/main/java/com/PokemonApp/SOAPPokemonService/application/service/PokemonService.java
@@ -5,19 +5,13 @@ import com.PokemonApp.SOAPPokemonService.domain.model.PokemonResponse;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PokemonService implements IPokemonService {
-    /*private final RestTemplate restTemplate = new RestTemplate();
-    private final String POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon";*/
+public class PokemonService implements IPokemonService {    
     private final IPokemonDAO pokemonDAO;
 
     public PokemonService(IPokemonDAO pokemonDAO) {
         this.pokemonDAO = pokemonDAO;
     }
-    /*@Override
-    public PokemonResponse getPokemons(int offset, int limit) {
-        String url = POKEAPI_URL + "?offset=" + offset + "&limit=" + limit;
-        return restTemplate.getForObject(url, PokemonResponse.class);
-    }*/
+    
     @Override
     public PokemonResponse getPokemons(int offset, int limit) {
         return pokemonDAO.getPokemons(offset, limit);
